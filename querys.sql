11d4dc15f8ee32ea53cecf77ff4014ff85dfb83f
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2017-11-12 00:08:09 -0200

    Adding address and phone number registers

--- querys.sql
+++ querys.sql
@@ -1,43 +1,168 @@
 -- Endereço --
-CREATE (pais:PAIS {
-  nome: ''
+
+-- País --
+CREATE (Brasil:PAIS {
+  nome: 'Brasil',
+  ddi: 55
 })
 
-CREATE (estado:ESTADO {
-  nome: '',
-  sigla: ''
+-- UF --
+CREATE (DistritoFederal:UF {
+  nome: 'Distrito Federal',
+  sigla: 'DF'
 })
 
-CREATE (cidade:CIDADE {
-  nome: ''
+CREATE (SaoPaulo:UF {
+  nome: 'Sao Paulo',
+  sigla: 'SP'
 })
 
-CREATE (bairro:BAIRRO {
-  nome: ''
+CREATE (SantaCatarina:UF {
+  nome: 'Santa Catarina',
+  sigla: 'SC'
 })
 
-CREATE (rua:RUA {
-  nome: ''
+-- Cidade --
+CREATE (Brasilia:CIDADE {
+  nome: 'Brasília'
 })
 
-CREATE (endereco:ENDERECO {
-  numero: 0,
-  cep: ''
+CREATE (BragancaPaulista:CIDADE {
+  nome: 'Braganca Paulista'
 })
+
+CREATE (Blumenau:CIDADE {
+  nome: 'Blumenau'
+})
+
+-- Bairro --
+-- DF --
+CREATE (Ceilandia:BAIRRO {
+  nome: 'Ceilandia'
+})
+
+CREATE (Planaltina:BAIRRO {
+  nome: 'Planaltina'
+})
+
+-- SP --
+CREATE (CampoNovo:BAIRRO {
+  nome: 'Campo Novo'
+})
+
+CREATE (JardimEuropa:BAIRRO {
+  nome: 'JardimEuropa'
+})
+
+-- SC --
+CREATE (Progresso:BAIRRO {
+  nome: 'Passo Mano'
+})
+
+CREATE (Progresso:BAIRRO {
+  nome: 'JardimEuropa'
+})
+
+-- Endereco --
+-- Ceilandia/DF --
+CREATE (EQNOBlocoC:ENDERECO {
+  linha1: 'Quadra EQNO 3-5 Bloco C'
+  complemento: ''
+  numero: 939,
+  cep: '72250513'
+})
+
+-- Planaltina/DF --
+CREATE (Quadra13Conjunto7:ENDERECO {
+  linha1: 'Quadra 13 Conjunto 7'
+  complemento: 'Casa'
+  numero: 310,
+  cep: '73355307'
+})
+
+-- Campo Novo/SP --
+CREATE (PracaManoelRamosdeLima:ENDERECO {
+  linha1: 'Praça Manoel Ramos de Lima'
+  complemento: ''
+  numero: 769,
+  cep: '71080025'
+})
+
+-- Jardim Europa/SP --
+CREATE (AlamedaPolonia:ENDERECO {
+  linha1: 'Alameda Polonia'
+  complemento: ''
+  numero: 28,
+  cep: '71083451'
+})
+
+-- PassoManso/SC --
+CREATE (RuaAlbertSchulz:ENDERECO {
+  linha1: 'RuaAlbertSchulz'
+  complemento: ''
+  numero: 632,
+  cep: '89032330'
+})
+
+-- Progresso/SC --
+CREATE (RuaOscarJosePering:ENDERECO {
+  linha1: 'Rua Oscar José Pering'
+  complemento: ''
+  numero: 694,
+  cep: '89027411'
+})
+
+-- Tipo Endereco --
+CREATE (casa:TIPO_ENDERECO {
+  nome: 'Casa'
+})
+
+CREATE (apartamento:TIPO_ENDERECO {
+  nome: 'Apartamento'
+})
+
 
 
 
 -- Telefone --
-CREATE (telefone:TELEFONE {
-  numero: '',
-  ddd: ''
+-- DF --
+CREATE (telDF1:TELEFONE {
+  numero: '984632764',
+  ddd: 61
+})
+
+CREATE (telDF2:TELEFONE {
+  numero: '33654231',
+  ddd: 61
+})
+
+-- SP --
+CREATE (telSP1:TELEFONE {
+  numero: '83654327',
+  ddd: 11
+})
+
+CREATE (telSP2:TELEFONE {
+  numero: '32764391',
+  ddd: 11
+})
+
+-- SC --
+CREATE (telSC1:TELEFONE {
+  numero: '83527901',
+  ddd: 49
+})
+
+CREATE (telSC2:TELEFONE {
+  numero: '83562834',
+  ddd: 47
 })
 
 
 
 
 -- Cooperativas --
-CREATE (cooperativa:COOPERATIVA {
+CREATE (BomSabor:COOPERATIVA {
   nome: 'Bom Sabor'
 })
 
