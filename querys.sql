de656efe6517a4e4f6fb9876e53b74879a63b51d
Baldissera <guilhermebaldissera@hotmail.com>
2017-11-12 00:07:29 -0200

    add alimentos, fornecedores, planos

--- querys.sql
+++ querys.sql
@@ -44,46 +44,39 @@ CREATE (cooperativa:COOPERATIVA {
 
 
 -- Planos --
-CREATE (plano:PLANO {
-  nome: '',
-  valor_mensal: ''
-})
-
-
-
-
--- Mensalidade --
-CREATE (mensalidade:MENSALIDADE:NAO_PAGA {
-  vencimento: ''
-})
-
-
+CREATE (plano_mensal:PLANO { nome: 'mensal', valor_mensal: 200 })
+CREATE (plano_trimestral:PLANO { nome: 'trimestral', valor_mensal: 180 })
+CREATE (plano_semestral:PLANO { nome: 'semestral', valor_mensal: 150 })
+CREATE (plano_anual:PLANO { nome: 'anual', valor_mensal: 110 })
 
 
 
 -- Alimentos --
-CREATE (alimento:ALIMENTO {
-  nome: '',
-  tipo: ''
-})
-
+CREATE (maca:ALIMENTO { nome: 'Maçã', tipo: 'fruta' })
+CREATE (banana:ALIMENTO { nome: 'Banana', tipo: 'fruta' })
+CREATE (cenoura:ALIMENTO { nome: 'Cenoura', tipo: 'verdura' })
+CREATE (alface:ALIMENTO { nome: 'Alface', tipo: 'folha' })
+CREATE (arroz:ALIMENTO { nome: 'Arroz', tipo: 'semente' })
+CREATE (cebola:ALIMENTO { nome: 'Cebola', tipo: 'verdura' })
+CREATE (couve:ALIMENTO { nome: 'Couve', tipo: 'folha' })
+CREATE (tangerina:ALIMENTO { nome: 'Tangerina', tipo: 'fruta' })
+CREATE (milho_verde:ALIMENTO { nome: 'Milho Verde', tipo: 'verdura' })
+CREATE (feijao:ALIMENTO { nome: 'Feijão', tipo: 'semente' })
+CREATE (batata:ALIMENTO { nome: 'Batata', tipo: 'verdura' })
+CREATE (uva:ALIMENTO { nome: 'Uva', tipo: 'fruta' })
+CREATE (morango:ALIMENTO { nome: 'Morango', tipo: 'fruta' })
+CREATE (abacate:ALIMENTO { nome: 'Abacate', tipo: 'fruta' })
+CREATE (melao:ALIMENTO { nome: 'Melão', tipo: 'fruta' })
 
 
 
 -- Associados --
-CREATE (associado:ASSOCIADO {
-  nome: '',
-  cpf: '',
-  rg: '',
-  filiacao: '',
-  sexo: '',
-  estado_civil: '',
-  email: '',
-  escolaridade: '',
-  cargo: ''
-})
+CREATE (associado:ASSOCIADO { nome: '', cpf: '', rg: '', filiacao: '', sexo: '', estado_civil: '', email: '', escolaridade: '', cargo: '' })
 
 
+
+-- Mensalidade --
+CREATE (mensalidade:MENSALIDADE:NAO_PAGA { vencimento: '' })
 
 
 
@@ -94,14 +87,12 @@ CREATE (lista_de_compra:LISTA_DE_COMPRA:NAO_PAGA {
 
 
 
-
 -- Fornecedores --
-CREATE (fornecedor:FORNECEDOR {
-  nome: '',
-  cnpj: ''
-})
-
-
+CREATE (seara:FORNECEDOR { nome: 'Seara', razao_social: 'Seara Alimentos S/a', cnpj: '02.914.460/0085-69' })
+CREATE (copeagro:FORNECEDOR { nome: 'Copeagro', razao_social: 'Copeagro - Cooperativa Agropecuaria De Tambau E Regiao', cnpj: '13.715.182/0001-29' })
+CREATE (brasnica:FORNECEDOR { nome: 'Brasnica', razao_social: 'Brasnica Frutas Tropicais Ltda', cnpj: '66.293.218/0009-47' })
+CREATE (frutas_real:FORNECEDOR { nome: 'Frutas Real', razao_social: 'Distribuidora De Frutas Real Ltda', cnpj: '80.727.928/0001-01' })
+CREATE (citricola_lucato:FORNECEDOR { nome: 'Citricola Lucato', razao_social: 'Citricola Lucato Comercial Importadora E Exportadora Ltda', cnpj: '00.589.220/0001-00' })
 
 
 
